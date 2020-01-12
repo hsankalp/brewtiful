12c7fe76e07f69312e01070f40b76e2100cdf3a5
Sankalp Heranjal <sankalp.heranjal@perficient.com>
2020-01-12 10:41:59 -0600

    Updates

---
+++ precache-manifest.af2053334790cd1440d0b701b4b34857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4bcd86eaa9e81537a5949be30bbbcd",
+    "url": "/brewtiful/index.html"
+  },
+  {
+    "revision": "9faa5b2bdd96d9b6abe6",
+    "url": "/brewtiful/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1f059108526b6f3070e6",
+    "url": "/brewtiful/static/css/main.781b84d5.chunk.css"
+  },
+  {
+    "revision": "9faa5b2bdd96d9b6abe6",
+    "url": "/brewtiful/static/js/2.08352fb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/brewtiful/static/js/2.08352fb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f059108526b6f3070e6",
+    "url": "/brewtiful/static/js/main.1bd28c13.chunk.js"
+  },
+  {
+    "revision": "081976637a2c28c74804",
+    "url": "/brewtiful/static/js/runtime-main.091e8359.js"
+  }
+]);
